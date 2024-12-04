64bcbccdd4acd20161565f52544b39f1ea838e40
hata-tomoyuki <h.tomo0602@gmail.com>
2024-12-05 06:39:11 +0900

    fix common layout component

--- src/components/MainWrapper.tsx
+++ src/components/MainWrapper.tsx
@@ -9,7 +9,7 @@ const MainWrapper = ({ children, pageTitle }: MainWrapperProps) => {
     return (
         <main>
             <section className="w-4/5 mx-auto py-12">
-                <h2 className="text-2xl">{pageTitle}</h2>
+                <h2 className="text-2xl mb-6">{pageTitle}</h2>
                 {children}
             </section>
         </main>
